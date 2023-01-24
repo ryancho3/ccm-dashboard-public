98ef5ab66183792b98ea2120e939f50b22a25756
Ryan Cho <ryanjhcho3@gmail.com>
2023-01-24 00:10:12 -0500

    upload to github

--- ccmDashboard/settings.py
+++ ccmDashboard/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m5c0r0$biq6jvn70*2=_f36h3o+z%hn%nn@^@lo*53c8dund_!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dev',
         'USER': 'admin',
-        'PASSWORD': 'devpassword',
+        'PASSWORD': '',
         'HOST': 'ccm-dashboad-dev-db.cvpbaqyu2pl4.us-east-1.rds.amazonaws.com',
         'PORT': '3306',
     }
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'prod',
         'USER': 'prod',
-        'PASSWORD': 'prodpassword',
+        'PASSWORD': '',
         'HOST': 'awseb-e-r6yf772bpr-stack-awsebrdsdatabase-ygjf9pdkzyua.cvpbaqyu2pl4.us-east-1.rds.amazonaws.com',
         'PORT': '3306',
     }
